583787369c67ff620d0c752274ce520650406c08
João Victor Miguel <joaovmiguel9@gmail.com>
2024-09-17 02:30:40 -0300

    Add files

---
+++ executar_calculadora.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt update #verifica se ha novas versoes de programas disponiveis para instalacao
+sudo apt install python3 #instala o python3
+
+#Adicone o caminho correto ate o arquivo "CalcuradoraEBAC.py" subistituindo o que vem apos "python3"
+python3 .../CalcuradoraEBAC.py
